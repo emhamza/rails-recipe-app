fee847a988335effac8c69db1e4d636b52bb53cc
emhamza <imumairhamza@gmail.com>
2023-08-01 18:17:04 +0500

    Update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   resources :users
   put '/recipes/:id/toggle_public', to: 'recipes#toggle_public', as: 'toggle_recipe_public'
 
-  root "foods#index"
+  # root "foods#index"
 
 end
